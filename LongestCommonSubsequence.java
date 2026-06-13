770343a83ecb92b1b73e7e73d71d1bf64db61ca7
kashwin180904-star <kashwin180904@gmail.com>
2026-06-13 15:37:00 +0530

    Save workspace changes

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class LongestCommonSubsequence{
+    public static void main(String[] args){
+        Scanner get=new Scanner(System.in);
+        String str1=get.nextLine();
+        String str2=get.nextLine();
+        int dp[][]=new int[str1.length()][str2.length()];
+        for(int i=1;i<str1.length();i++){
+            for(int j=1;j<str2.length();j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        System.out.print(dp[str1.length()-1][str2.length()-1]);
+    }
+}
